c5f48090d459bd16c2ebcdb67dcd2a37323b4c18
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-11-22 23:45:59 +0300

    fix check expired

--- src/business/helpers/checkIsExpired.ts
+++ src/business/helpers/checkIsExpired.ts
@@ -1,7 +1,11 @@
 import dayjs from "dayjs";
 
+//если не установить время, но берем время до конца дня
 export const checkIsExpired = (currData: { date: string; time?: string }) => {
-  const dayString = currData?.date + currData?.time;
+  const defaultTime = "24:00";
+  const dayString = currData?.time
+    ? currData?.date + currData?.time
+    : currData?.date + defaultTime;
   const endDateTimeStamp = dayjs(dayString).format();
 
   const currTimeStamp = dayjs().format();
